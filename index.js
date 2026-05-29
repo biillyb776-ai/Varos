f0d37afb5b203b5e6d45e9b0b4867abfc4bb4734
biillyb776-ai <biillyb776@gmail.com>
2026-05-29 16:23:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
-// Bir önceki adımda hazırladığımız bot.js dosyasını çağırıyoruz
+// bot.js dosyasındaki ana motoru çağırıyoruz
 const startMyBot = require('./bot.js'); 
 
-// Botun ayarlarını ve şifreni tam olarak buradan veriyorsun kanka:
+// Senin verdiğin 6b6t giriş bilgileri ve şifren kanka:
 startMyBot({
   host: '6b6t.org',
   port: 25565,
   username: 'VuadasTpaBot1',
-  password: 'Ewdry3NgAF6h9', // Şifren güvenli şekilde buraya bağlandı
+  password: 'Ewdry3NgAF6h9', 
   reconnectDelay: 60000       // Bağlantı koparsa 1 dakika bekleyip otomatik bağlanır
 });
 
